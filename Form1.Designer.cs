7e686f58ef73a1fe2b52462d6a5abe44454a2afa
= <=>
2017-10-27 19:05:35 +0300

    lab4

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,16 @@
 ﻿namespace laba2
 {
-    partial class Form1
+    partial class FormAerodrome
     {
         /// <summary>
-        /// Обязательная переменная конструктора.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Освободить все используемые ресурсы.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,134 +20,170 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBoxParking = new System.Windows.Forms.PictureBox();
-            this.buttonSetCar = new System.Windows.Forms.Button();
-            this.buttonSetSportCar = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBoxAerodrome = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.pictureBoxTakeCar = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.butTake = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).BeginInit();
+            this.pictureBoxTakePlane = new System.Windows.Forms.PictureBox();
+            this.butSetPlane = new System.Windows.Forms.Button();
+            this.butSetFigther = new System.Windows.Forms.Button();
+            this.listBoxLevels = new System.Windows.Forms.ListBox();
+            this.btnLevelDown = new System.Windows.Forms.Button();
+            this.btnLevelUp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAerodrome)).BeginInit();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakePlane)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBoxParking
+            // pictureBoxAerodrome
             // 
-            this.pictureBoxParking.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.pictureBoxParking.Location = new System.Drawing.Point(17, 10);
-            this.pictureBoxParking.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBoxParking.Name = "pictureBoxParking";
-            this.pictureBoxParking.Size = new System.Drawing.Size(852, 520);
-            this.pictureBoxParking.TabIndex = 3;
-            this.pictureBoxParking.TabStop = false;
-            // 
-            // buttonSetCar
-            // 
-            this.buttonSetCar.Location = new System.Drawing.Point(896, 28);
-            this.buttonSetCar.Name = "buttonSetCar";
-            this.buttonSetCar.Size = new System.Drawing.Size(117, 41);
-            this.buttonSetCar.TabIndex = 4;
-            this.buttonSetCar.Text = "Пришвартовать корабль";
-            this.buttonSetCar.UseVisualStyleBackColor = true;
-            this.buttonSetCar.Click += new System.EventHandler(this.buttonSetCar_Click);
-            // 
-            // buttonSetSportCar
-            // 
-            this.buttonSetSportCar.Location = new System.Drawing.Point(896, 75);
-            this.buttonSetSportCar.Name = "buttonSetSportCar";
-            this.buttonSetSportCar.Size = new System.Drawing.Size(117, 56);
-            this.buttonSetSportCar.TabIndex = 5;
-            this.buttonSetSportCar.Text = "Пришвартовать UltaMegaBuffSuperMotorShip";
-            this.buttonSetSportCar.UseVisualStyleBackColor = true;
-            this.buttonSetSportCar.Click += new System.EventHandler(this.buttonSetSportCar_Click_1);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 38);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Место:";
+            this.pictureBoxAerodrome.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBoxAerodrome.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBoxAerodrome.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxAerodrome.Name = "pictureBoxAerodrome";
+            this.pictureBoxAerodrome.Size = new System.Drawing.Size(862, 453);
+            this.pictureBoxAerodrome.TabIndex = 0;
+            this.pictureBoxAerodrome.TabStop = false;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.pictureBoxTakeCar);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.butTake);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.maskedTextBox1);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Location = new System.Drawing.Point(899, 137);
+            this.groupBox1.Controls.Add(this.pictureBoxTakePlane);
+            this.groupBox1.Location = new System.Drawing.Point(868, 255);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(171, 369);
-            this.groupBox1.TabIndex = 9;
+            this.groupBox1.Size = new System.Drawing.Size(187, 198);
+            this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Отчалить!";
+            this.groupBox1.Text = "Теплоход";
             // 
-            // pictureBoxTakeCar
+            // butTake
             // 
-            this.pictureBoxTakeCar.Location = new System.Drawing.Point(15, 93);
-            this.pictureBoxTakeCar.Name = "pictureBoxTakeCar";
-            this.pictureBoxTakeCar.Size = new System.Drawing.Size(150, 270);
-            this.pictureBoxTakeCar.TabIndex = 11;
-            this.pictureBoxTakeCar.TabStop = false;
+            this.butTake.Location = new System.Drawing.Point(68, 162);
+            this.butTake.Name = "butTake";
+            this.butTake.Size = new System.Drawing.Size(104, 30);
+            this.butTake.TabIndex = 3;
+            this.butTake.Text = "Забрать";
+            this.butTake.UseVisualStyleBackColor = true;
+            this.butTake.Click += new System.EventHandler(this.butTake_Click);
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(9, 64);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Забрать";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Место";
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(45, 38);
+            this.maskedTextBox1.Location = new System.Drawing.Point(68, 31);
             this.maskedTextBox1.Mask = "00";
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(48, 20);
-            this.maskedTextBox1.TabIndex = 9;
+            this.maskedTextBox1.Size = new System.Drawing.Size(67, 20);
+            this.maskedTextBox1.TabIndex = 1;
             // 
-            // Form1
+            // pictureBoxTakePlane
+            // 
+            this.pictureBoxTakePlane.Location = new System.Drawing.Point(6, 67);
+            this.pictureBoxTakePlane.Name = "pictureBoxTakePlane";
+            this.pictureBoxTakePlane.Size = new System.Drawing.Size(175, 89);
+            this.pictureBoxTakePlane.TabIndex = 0;
+            this.pictureBoxTakePlane.TabStop = false;
+            // 
+            // butSetPlane
+            // 
+            this.butSetPlane.Location = new System.Drawing.Point(883, 155);
+            this.butSetPlane.Name = "butSetPlane";
+            this.butSetPlane.Size = new System.Drawing.Size(166, 43);
+            this.butSetPlane.TabIndex = 2;
+            this.butSetPlane.Text = "Спустить MotorShip";
+            this.butSetPlane.UseVisualStyleBackColor = true;
+            this.butSetPlane.Click += new System.EventHandler(this.butSetPlane_Click);
+            // 
+            // butSetFigther
+            // 
+            this.butSetFigther.Location = new System.Drawing.Point(883, 204);
+            this.butSetFigther.Name = "butSetFigther";
+            this.butSetFigther.Size = new System.Drawing.Size(166, 45);
+            this.butSetFigther.TabIndex = 3;
+            this.butSetFigther.Text = "Спустить UltaMegaBuffSuperMotorShip";
+            this.butSetFigther.UseVisualStyleBackColor = true;
+            this.butSetFigther.Click += new System.EventHandler(this.butSetFigther_Click);
+            // 
+            // listBoxLevels
+            // 
+            this.listBoxLevels.FormattingEnabled = true;
+            this.listBoxLevels.Location = new System.Drawing.Point(888, 0);
+            this.listBoxLevels.Name = "listBoxLevels";
+            this.listBoxLevels.Size = new System.Drawing.Size(167, 69);
+            this.listBoxLevels.TabIndex = 4;
+            // 
+            // btnLevelDown
+            // 
+            this.btnLevelDown.Location = new System.Drawing.Point(887, 75);
+            this.btnLevelDown.Name = "btnLevelDown";
+            this.btnLevelDown.Size = new System.Drawing.Size(81, 28);
+            this.btnLevelDown.TabIndex = 5;
+            this.btnLevelDown.Text = "<<";
+            this.btnLevelDown.UseVisualStyleBackColor = true;
+            this.btnLevelDown.Click += new System.EventHandler(this.btnLevelDown_Click);
+            // 
+            // btnLevelUp
+            // 
+            this.btnLevelUp.Location = new System.Drawing.Point(974, 75);
+            this.btnLevelUp.Name = "btnLevelUp";
+            this.btnLevelUp.Size = new System.Drawing.Size(81, 28);
+            this.btnLevelUp.TabIndex = 6;
+            this.btnLevelUp.Text = ">>";
+            this.btnLevelUp.UseVisualStyleBackColor = true;
+            this.btnLevelUp.Click += new System.EventHandler(this.btnLevelUp_Click);
+            // 
+            // FormAerodrome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1076, 587);
+            this.ClientSize = new System.Drawing.Size(1067, 453);
+            this.Controls.Add(this.btnLevelUp);
+            this.Controls.Add(this.btnLevelDown);
+            this.Controls.Add(this.listBoxLevels);
+            this.Controls.Add(this.butSetFigther);
+            this.Controls.Add(this.butSetPlane);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.buttonSetSportCar);
-            this.Controls.Add(this.buttonSetCar);
-            this.Controls.Add(this.pictureBoxParking);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "Form1";
+            this.Controls.Add(this.pictureBoxAerodrome);
+            this.Name = "FormAerodrome";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAerodrome)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakePlane)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBoxParking;
-		private System.Windows.Forms.Button buttonSetCar;
-		private System.Windows.Forms.Button buttonSetSportCar;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.GroupBox groupBox1;
-		private System.Windows.Forms.PictureBox pictureBoxTakeCar;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-	}
-}
 
+        private System.Windows.Forms.PictureBox pictureBoxAerodrome;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button butTake;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.PictureBox pictureBoxTakePlane;
+        private System.Windows.Forms.Button butSetPlane;
+        private System.Windows.Forms.Button butSetFigther;
+        private System.Windows.Forms.ListBox listBoxLevels;
+        private System.Windows.Forms.Button btnLevelDown;
+        private System.Windows.Forms.Button btnLevelUp;
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -10,67 +10,102 @@ using System.Windows.Forms;
 
 namespace laba2
 {
-	public partial class Form1 : Form
+	public partial class FormAerodrome : Form
 	{
 		Parking parking;
 
-		public Form1()
+
+
+		public FormAerodrome()
 		{
 			InitializeComponent();
-			parking = new Parking();
-			Draw();
+            parking = new Parking(5);
+			for (int i = 1; i < 6; i++)
+			{
+				listBoxLevels.Items.Add("Уровень " + i);
+			}
+			listBoxLevels.SelectedIndex = parking.getCurentLvl;
+			
+			draw();
 		}
 
-		private void Draw()
+		public void draw()
 		{
-			Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
-			Graphics gr = Graphics.FromImage(bmp);
-			parking.Draw(gr, pictureBoxParking.Width, pictureBoxParking.Height);
-			pictureBoxParking.Image = bmp;
+			if (listBoxLevels.SelectedIndex > -1)
+			{
+				Bitmap bmp = new Bitmap(pictureBoxAerodrome.Width, pictureBoxAerodrome.Height);
+				Graphics gr = Graphics.FromImage(bmp);
+                parking.Draw(gr);
+				pictureBoxAerodrome.Image = bmp;
+			}
 		}
 
-		private void buttonSetCar_Click(object sender, EventArgs e)
+
+
+		private void butSetPlane_Click(object sender, EventArgs e)
 		{
 			ColorDialog dialog = new ColorDialog();
 			if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
 			{
-				var car = new MotorShip(100, 4, 1000, dialog.Color);
-				int place = parking.PutShipInParking(car);
-				Draw();
-				MessageBox.Show("Вашеместо: " + (place + 1));
+				var plane = new MotorShip(1000, 100, 30, dialog.Color);
+                int place = parking.PutShipInParking(plane);
+				draw();
+				MessageBox.Show("Вашеместо: " + place);
+
 			}
 		}
 
-		private void button1_Click(object sender, EventArgs e)
+		private void butSetFigther_Click(object sender, EventArgs e)
 		{
-
-			if (maskedTextBox1.Text != "")
+			ColorDialog dialog = new ColorDialog();
+			if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
 			{
-				ITransport car = parking.GetShipInParking(((Convert.ToInt32(maskedTextBox1.Text)) - 1));
+				ColorDialog dialogDop = new ColorDialog();
+				if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				{
+					var plane = new UltaMegaBuffSuperMotorShip(1000, 100, 30, dialog.Color, true, true, dialogDop.Color);
+                    int place = parking.PutShipInParking(plane);
 
-				Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width, pictureBoxTakeCar.Height);
-				Graphics gr = Graphics.FromImage(bmp);
-				car.sePosition(0, 100);
-				car.drawShip(gr);
-				pictureBoxTakeCar.Image = bmp;
-				Draw();
+                    draw();
+                    MessageBox.Show("Вашеместо: " + (place + 1));
+
+				}
+			}
+		}
+		
+		private void butTake_Click(object sender, EventArgs e)
+		{
+			if (listBoxLevels.SelectedIndex > -1)
+			{
+				string level = listBoxLevels.Items[listBoxLevels.SelectedIndex].ToString();
+
+				if (maskedTextBox1.Text != "")
+				{
+                    var plane = parking.GetShipInParking(Convert.ToInt32(maskedTextBox1.Text) -1 );
+					Bitmap bmp = new Bitmap(pictureBoxTakePlane.Width, pictureBoxTakePlane.Height);
+					Graphics gr = Graphics.FromImage(bmp);
+					plane.sePosition(45, 50);
+					plane.drawShip(gr);
+					pictureBoxTakePlane.Image = bmp;
+					draw();
+				}
 			}
 		}
 
-        private void buttonSetSportCar_Click_1(object sender, EventArgs e)
-        {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                ColorDialog dialogDop = new ColorDialog();
-                if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                {
-                    var car = new UltaMegaBuffSuperMotorShip(100, 4, 1000, dialog.Color, true, true, dialogDop.Color);
-                    int place = parking.PutShipInParking(car);
-                    Draw();
-                    MessageBox.Show("Вашеместо: " + (place + 1));
-                }
-            }
-        }
-    }
+		private void btnLevelDown_Click(object sender, EventArgs e)
+		{
+            parking.lvlDown();
+			listBoxLevels.SelectedIndex = parking.getCurentLvl;
+			draw();
+
+		}
+
+		private void btnLevelUp_Click(object sender, EventArgs e)
+		{
+            parking.lvlUp();
+			listBoxLevels.SelectedIndex = parking.getCurentLvl;
+			draw();
+
+		}
+	}
 }
