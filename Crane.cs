2c8bbbc6d3fac5fd1e85774336f4cf4f64bb34b1
= <=>
2017-10-27 12:32:08 +0300

    lab1 ref

---
+++ Crane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs
+{
+	class Crane
+	{
+		public void pomblt(Egg egg)
+		{
+			egg.isClean = true;
+		}
+	}
+}
